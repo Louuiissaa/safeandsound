460f09de948922aa8759c0b298b3350616e10d5c
Dominic Steinhauser <dominic_steinhauser@t-online.de>
2017-05-19 16:08:18 +0200

    Gas sensor werte anzeigen & Livestream

--- app/src/main/java/com/safeandsound/app/safeandsound/view/AirQualityActivity.java
+++ app/src/main/java/com/safeandsound/app/safeandsound/view/AirQualityActivity.java
@@ -13,6 +13,7 @@ import com.safeandsound.app.safeandsound.R;
 import com.safeandsound.app.safeandsound.controller.database.SQLiteHandler;
 import com.safeandsound.app.safeandsound.SessionManager;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,10 +21,11 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 /**
- * Created by louisapabst on 16.04.17.
+ * Created by louisapabst, Dominic on 16.04.17.
  */
 
 public class AirQualityActivity extends FragmentActivity {
@@ -51,7 +53,7 @@ public class AirQualityActivity extends FragmentActivity {
         InputStream is = null;
         StringBuilder sb=null;
 
-        //http post
+        //http get
         try {
             URL urlObj = new URL(AppConfig.URL_TEMP);
             result = new ConnectionFeed().execute(urlObj.toString()).get();
@@ -67,6 +69,9 @@ public class AirQualityActivity extends FragmentActivity {
         //paring data
         String fd_temp = "";
         String fd_humidity = "";
+        String fd_co2 = "";
+        String fd_co = "";
+        String fd_nh4 = "";
         try{
             result = result.replace('"','\'');
             JSONObject json_data= new JSONObject(result);
@@ -78,13 +83,66 @@ public class AirQualityActivity extends FragmentActivity {
             e1.printStackTrace();
         }
 
+        result = null;
+        is = null;
+        sb=null;
+
+        /*
+        *    Get-Request um die aktuellen daten des Mq-135 sensors auszulesen ********
+        *
+        */
+
+        try {
+            URL urlObj = new URL(AppConfig.URL_MQGas);
+            result = new ConnectionFeed().execute(urlObj.toString()).get();
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }catch (ExecutionException e){
+            e.printStackTrace();
+        }
+
+
+        try{
+            result = result.replace('"','\'');
+            JSONArray resultArray = new JSONArray( result);
+            JSONObject json_data= (JSONObject)resultArray.getJSONObject(0);
+            fd_co2 = json_data.getString("CO2");
+            fd_co = json_data.getString("CO");
+            fd_nh4 = json_data.getString("NH4");
+        }catch(JSONException e1){
+            Toast.makeText(getBaseContext(), "No Data Found", Toast.LENGTH_LONG).show();
+        }catch (ParseException e1){
+            e1.printStackTrace();
+        }
+
+
+        //CO2, CO, NH4 Werte runden
+        fd_co2 = String.format(Locale.GERMAN, "%.3f", Double.parseDouble(fd_co2));
+        fd_co = String.format(Locale.GERMAN, "%.3f", Double.parseDouble(fd_co));
+        fd_nh4 = String.format(Locale.GERMAN, "%.3f", Double.parseDouble(fd_nh4));
+
+
         //print temperature
         TextView text_temperature = (TextView)findViewById(R.id.temperatureOutputText);
-        text_temperature.setText(fd_temp);
+        text_temperature.setText(fd_temp+ "°C");
 
         //print Humidity
         TextView text_humidity = (TextView)findViewById(R.id.humidityOutputText);
-        text_humidity.setText(fd_humidity);
+        text_humidity.setText(fd_humidity +"%");
+
+        //print CO2
+        TextView text_co2 = (TextView)findViewById(R.id.carbondioxidOutputText);
+        text_co2.setText(fd_co2 + " ppm");
+
+        //print CO
+        TextView text_co = (TextView)findViewById(R.id.carbonmonoxidOutputText);
+        text_co.setText(fd_co + " ppm");
+
+        //print NH4 carbonmonoxidOutputText
+        TextView text_nh = (TextView)findViewById(R.id.oxygenOutputText);
+        text_nh.setText(fd_nh4 + " ppm");
     }
 
     //Meldet den aktuellen Nutzer ab
--- app/src/main/java/com/safeandsound/app/safeandsound/view/LiveStreamActivity.java
+++ app/src/main/java/com/safeandsound/app/safeandsound/view/LiveStreamActivity.java
@@ -2,10 +2,11 @@ package com.safeandsound.app.safeandsound.view;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebView;
-
+import com.safeandsound.app.safeandsound.AppConfig;
 import com.safeandsound.app.safeandsound.R;
+
+
 
 /**
  * Created by Dominic on 27.04.2017.
@@ -20,8 +21,25 @@ public class LiveStreamActivity extends Activity {
         setContentView(R.layout.livestream);
 
         webView  = (WebView)findViewById(R.id.webView2);
+        //setzte den Webviw auf die größe vom smartphone
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
         //Motion muss auf dem Raspberry laufen, damit der Livestream angesehen werden kann.
-        webView.loadUrl("http://192.168.2.103:8081");
+
+        //get the URL of the Livestream
+        String urlLivestream = AppConfig.URL_LIVESTREAM;
+
+        //access livestream and show it in a webView
+        webView.loadUrl(urlLivestream);
+
+
+    }
+
+
+    //'Zurück' Navigation schließt die Verbindung und den View
+    public void onPause(){
+        super.onPause();
+        webView.destroy();
     }
 
 
