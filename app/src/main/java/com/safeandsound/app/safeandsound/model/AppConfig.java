a7cb0d6903b60c32feefe85d5c42fd543e44604b
Luisa Papst <lpabst@hotmail.de>
2017-05-30 20:12:46 +0200

    IP Anpassung in AppConfig

--- app/src/main/java/com/safeandsound/app/safeandsound/model/AppConfig.java
+++ app/src/main/java/com/safeandsound/app/safeandsound/model/AppConfig.java
@@ -5,44 +5,44 @@ package com.safeandsound.app.safeandsound.model;
  */
 
 public class AppConfig {
-        private static String ipAddressRP;
+        private static String ip = "http://192.168.10.53";
         // Server user login url
-        public static String URL_LOGIN = "http://" + ipAddressRP + "/db_login.php";
+        public static String URL_LOGIN = "http://" + ip + "/db_login.php";
 
         // Server user register url
-        public static String URL_SIGNUP = "http://" + ipAddressRP + "/db_register.php";
+        public static String URL_SIGNUP = "http://" + ip + "/db_register.php";
 
         // Server user temperature url
-        public static String URL_TEMP = "http://" + ipAddressRP + "/temp";
+        public static String URL_TEMP = "http://" + ip + "/temp";
 
         // Server user window url
-        public static String URL_WINDOW = "http://" + ipAddressRP + "/window";
+        public static String URL_WINDOW = "http://" + ip + "/window";
 
         // Server user livestream url
-        public static String URL_LIVESTREAM = "http://" + ipAddressRP + ":8081";
+        public static String URL_LIVESTREAM = "http://" + ip + ":8081";
 
         // Server user motion url
-        public static String URL_MOTION = "http://" + ipAddressRP + "/motion";
+        public static String URL_MOTION = "http://" + ip + "/motion";
 
         // Server MQGas url
-        public static String URL_MQGas = "http://" + ipAddressRP + ":1880/mqgas";
+        public static String URL_MQGas = "http://" + ip + ":1880/mqgas";
 
         //Server Temp Intervall url
-        public static String URL_TEMPINT = "http://" + ipAddressRP + ":1880/tempInt?from=";
+        public static String URL_TEMPINT = "http://" + ip + ":1880/tempInt?from=";
 
         //Server MQ Gas Intervall url
-        public static String URL_MQGasInt = "http://" + ipAddressRP + ":1880/gasInt?from=";
+        public static String URL_MQGasInt = "http://" + ip + ":1880/gasInt?from=";
 
         //Server Motion Picture url
-        public static String URL_MOTION_PICTURE = "http://" + ipAddressRP + ":1880/getPic?id=";
+        public static String URL_MOTION_PICTURE = "http://" + ip + ":1880/getPic?id=";
 
 
 
-        public String getIpAddressRP() {
-                return ipAddressRP;
+        public String getIp() {
+                return ip;
         }
 
-        public void setIpAddressRP(String ipAddressRP) {
-                ipAddressRP = ipAddressRP;
+        public void setIp(String ip) {
+                ip = ip;
         }
 }
